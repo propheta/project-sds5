862d06a5df3f18a04b4b519b9788048121daa346
Gabriel Bolea <gabriel.a.bolea@gmail.com>
2021-11-06 23:41:40 +0000

    Group by search

--- backend/src/main/java/com/gabrielbolea/dsvendas/repositories/SaleRepository.java
+++ backend/src/main/java/com/gabrielbolea/dsvendas/repositories/SaleRepository.java
@@ -1,10 +1,23 @@
 package com.gabrielbolea.dsvendas.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.gabrielbolea.dsvendas.dto.SaleSuccessDTO;
+import com.gabrielbolea.dsvendas.dto.SaleSumDTO;
 import com.gabrielbolea.dsvendas.entities.Sale;
 
 public interface SaleRepository extends JpaRepository<Sale, Long> {
 
+	@Query("SELECT new com.gabrielbolea.dsvendas.dto.SaleSumDTO(obj.seller, SUM(obj.amount)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSumDTO> amountGroupedBySeller();
+	
+	@Query("SELECT new com.gabrielbolea.dsvendas.dto.SaleSuccessDTO(obj.seller, SUM(obj.visited), SUM(obj.deals)) "
+			+ "FROM Sale AS obj GROUP BY obj.seller")
+	List<SaleSuccessDTO> successGroupedBySeller();
+	
 }
 
