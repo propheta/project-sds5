f4da3b87e37537cf0773e82b62a04ab90bf92f8a
Gabriel Bolea <gabriel.a.bolea@gmail.com>
2021-11-07 10:26:02 +0000

    Home, navigation

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,9 +1,30 @@
-const Home = () => {
-    return (
-        <div>
-            <h1>Página Home</h1>
-        </div>
-    )
-}
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
+import { Link } from "react-router-dom";
 
-export default Home
+const Home = () => {
+  return (
+    <>
+      <NavBar />
+      <div className="container">
+        <div className="jumbotron">
+          <h1 className="display-4">DSVendas</h1>
+          <p className="lead">
+            Analise o desempenho das suas vendas por diferentes perspectivas
+          </p>
+          <hr />
+          <p>
+            Esta aplicação consiste em mostrar um dashboard a partir de dados
+            fornecidos por um back end construído com Spring Boot.
+          </p>
+          <Link className="btn btn-primary btn-lg" to="/dashboard">
+            Acesso ao Dashboard
+          </Link>
+        </div>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default Home;
